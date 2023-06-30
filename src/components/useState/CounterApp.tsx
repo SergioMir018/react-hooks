a512e0fdf68fa1f4ce10e32b6c372494331052b6
Sergio Mir <voidtracer6@gmail.com>
2023-06-29 21:17:04 -0400

    ./scr/components/useState/CounterApp.tsx: styles added

--- src/components/useState/CounterApp.tsx
+++ src/components/useState/CounterApp.tsx
@@ -6,8 +6,8 @@ export const CounterApp  = () => {
 
   return (
     <div className='text-white flex flex-col items-center'>
-      <div className='font-bold text-3xl m-3'>
-        Counter { counter }
+      <div className='font-bold text-3xl m-3 underline decoration-dashed underline-offset-4'>
+        Counter: <span className='ml-1'> { counter } </span>
       </div>
       <button onClick={ () => setCounter( counter + 1 ) }
       className=' bg-slate-600 border-4 p-3 rounded-md hover:bg-slate-200 hover:text-black hover:rounded-2xl hover:transition-all duration-500 ease-in-out font-semibold text-lg'
