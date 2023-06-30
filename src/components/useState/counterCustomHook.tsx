0b64db1fbbf4b0e5368aa80a2516c3ee472f7a95
Sergio Mir <voidtracer6@gmail.com>
2023-06-30 02:12:27 -0400

    ./scr/components/useState/customCounterHook.tsx & ./scr/hooks/useCounter.ts: new function added to the custom hook and used on the component

--- src/components/useState/counterCustomHook.tsx
+++ src/components/useState/counterCustomHook.tsx
@@ -4,13 +4,14 @@ import { useCounter } from '../../hooks/useCounter.ts';
 
 export const CounterCustomHook = () => {
 
-  const { state, increment, decrement } = useCounter();
+  const { state, increment, decrement, reset } = useCounter();
 
   return (
     <div className='text-white flex flex-col items-center'>
       <h1  className='font-bold text-3xl m-3 underline decoration-dashed underline-offset-4'>Counter: { state }</h1>
       <div>
         <button onClick={ increment } className=' bg-slate-600 border-4 mr-4 p-3 rounded-md hover:bg-slate-200 hover:text-black hover:rounded-3xl hover:transition-all duration-500 ease-in-out font-semibold text-lg active:bg-slate-400'> + 1 </button>
+        <button onClick={ reset } className=' bg-slate-600 border-4 mr-4 p-3 rounded-md hover:bg-slate-200 hover:text-black hover:rounded-3xl hover:transition-all duration-500 ease-in-out font-semibold text-lg active:bg-slate-400'> Reset </button>
         <button onClick={ decrement } className=' bg-slate-600 border-4 py-3 px-4 rounded-md hover:bg-slate-200 hover:text-black hover:rounded-3xl hover:transition-all duration-500 ease-in-out font-semibold text-lg active:bg-slate-400'> - 1 </button>
       </div>
       
--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -4,10 +4,11 @@ export const useCounter = ( initialState: number = 0 ) => {
 
   const [state, setState] = useState<number>( initialState );
 
-
   const increment = () => setState( state + 1 );
 
   const decrement = () => setState( state - 1 );
 
-  return { state, increment, decrement };
+  const reset = () => setState( initialState );
+
+  return { state, increment, decrement, reset};
 }
