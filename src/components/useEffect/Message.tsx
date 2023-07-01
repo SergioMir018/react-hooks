081c0b825bcac8297c08f1f1c41f4a155d936c4f
Sergio Mir <voidtracer6@gmail.com>
2023-07-01 13:29:14 -0400

    ./scr/components/useEffect/Message.tsx: new useEffect added to the component, the coordenates of the mouse will be showned on screen

--- src/components/useEffect/Message.tsx
+++ src/components/useEffect/Message.tsx
@@ -1,12 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+
+interface Coords { 
+  x: number, 
+  y: number
+}
+
 
 const Message = () => {
 
+  const [coords, setCoords] = useState<Coords>({ x: 0, y: 0 });
+
+  const { x, y } = coords;
+
   useEffect(() => {
-    console.log('component rendered'); 
+
+    const mouseMove = (event: MouseEvent) => {
+      const coords = { x: event.x, y: event.y};
+
+      setCoords(coords);
+    };
+
+    window.addEventListener('mousemove', mouseMove);
   
     return () => {
-      console.log('component not rendered');
+      window.removeEventListener('mousemove', mouseMove);
     }
   }, []);
   
@@ -14,6 +31,9 @@ const Message = () => {
   return (
     <div className='mt-2 text-white'>
       <h3 className='text-5xl'> This is great! </h3>
+      <p className='mt-3 text-center'>
+        x:{ x } y:{ y }
+      </p>
     </div>
   )
 }
