c118c4e75771d1d368cf7a725f59478bc2c737bd
Sergio Mir <voidtracer6@gmail.com>
2023-07-05 23:06:18 -0400

    ./scr/hooks/useFetch.ts: added and implemented

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,5 +1,32 @@
-const useFetch = () => {
-  const [state, setState] = useState( data )
+import { useEffect, useState } from 'react';
+import { BreakingBadAPI } from '../types';
+
+interface StateBreakingBadAPI {
+  data: BreakingBadAPI[] | null
+  loading: boolean
+  error: Error | null
+}
+
+const useFetch = ( url: string ) => {
+  const [state, setState] = useState<StateBreakingBadAPI>({
+    data: null, 
+    loading: true, 
+    error: null
+  });
+
+  useEffect(() => {
+    fetch( url )
+      .then( response => response.json() )
+      .then( responseData => {
+        setState({
+          data: responseData,
+          loading: false,
+          error: null
+        })
+      });
+  }, [ url ]);
+
+  return state;
 }
 
 export default useFetch
