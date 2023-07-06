a809791237b91854c06105b7b4b44d37cadf58e1
Sergio Mir <voidtracer6@gmail.com>
2023-07-05 23:28:02 -0400

    ./scr/components/QuoteCard/QuoteCard.tsx: added

--- src/components/MultipleCustomHooks/MultipleCustomHooks.tsx
+++ src/components/MultipleCustomHooks/MultipleCustomHooks.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import useFetch from '../../hooks/useFetch'
+import QuoteCard from '../QuoteCard/QuoteCard';
 
 const MultipleCustomHooks = () => {
+
+  const fetchState = useFetch( `https://api.breakingbadquotes.xyz/v1/quotes/1` );
+  console.log( fetchState );
+
   return (
-    <div className='flex justify-center'>
-      <h1 className='text-white mt-5 text-3xl font-semibold'> Custom Hooks </h1>
+    <div className='flex flex-col items-center'>
+      <h1 className='text-white mt-5 text-3xl font-semibold'> Breaking Bad Quotes </h1>
+      <br />
+      <div className='mt-4'>
+        <h2 className='text-white animate-pulse animate-infinite animate-duration-[900ms] animate-ease-in-out'> Loading... </h2>
+      </div>
+      <QuoteCard />
     </div>
   )
 }
---
+++ src/components/QuoteCard/QuoteCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const QuoteCard = () => {
+  return (
+    <div>
+      <blockquote className='text-end text-white'>
+        <p className='text-xl'>
+          Hola Mundo
+        </p>
+        <footer className='text-lg'> - Author</footer>
+      </blockquote>
+    </div>
+  )
+}
+
+export default QuoteCard
