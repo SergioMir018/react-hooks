9ca1c5acffb96a7d57154a025de493bac96be2f6
Sergio Mir <voidtracer6@gmail.com>
2023-07-05 22:35:53 -0400

    ./scr/components/MultipleCustomHooks/MultipleCustomHooks.tsx: added

--- src/HookApp.tsx
+++ src/HookApp.tsx
@@ -3,12 +3,13 @@ import { CounterApp } from './components/useState/CounterApp.tsx';
 import { CounterCustomHook } from './components/useState/counterCustomHook.tsx';
 import { SimpleForm } from './components/useEffect/SimpleForm.tsx';
 import { CustomHookSimpleForm } from './components/useEffect/CustomHookSimpleForm.tsx';
+import MultipleCustomHooksComponent from './components/MultipleCustomHooks/MultipleCustomHooks.tsx';
 
 const HookApp = () => {
 
   return (
     <div>
-      <CustomHookSimpleForm />
+      <MultipleCustomHooksComponent />
     </div>
   )
 }
---
+++ src/components/MultipleCustomHooks/MultipleCustomHooks.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MultipleCustomHooks = () => {
+  return (
+    <div className='flex justify-center'>
+      <h1 className='text-white mt-5 text-3xl font-semibold'> Custom Hooks </h1>
+    </div>
+  )
+}
+
+export default MultipleCustomHooks
