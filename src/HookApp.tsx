cd94e5a7da3ee74b0e739c07d0495edfca5c4561
Sergio Mir <voidtracer6@gmail.com>
2023-07-06 13:49:42 -0400

    ./docs: new build for prod

--- src/HookApp.tsx
+++ src/HookApp.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { CounterApp } from './components/useState/CounterApp.tsx';
-import { CounterCustomHook } from './components/useState/counterCustomHook.tsx';
-import { SimpleForm } from './components/useEffect/SimpleForm.tsx';
-import { CustomHookSimpleForm } from './components/useEffect/CustomHookSimpleForm.tsx';
+// import { CounterApp } from './components/useState/CounterApp.tsx';
+// import { CounterCustomHook } from './components/useState/counterCustomHook.tsx';
+// import { SimpleForm } from './components/useEffect/SimpleForm.tsx';
+// import { CustomHookSimpleForm } from './components/useEffect/CustomHookSimpleForm.tsx';
 import MultipleCustomHooksComponent from './components/MultipleCustomHooks/MultipleCustomHooks.tsx';
 
 const HookApp = () => {
