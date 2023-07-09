8ffa7ba67fb106a1c9d413a987d4a0efaa9a2a1f
Sergio Mir <voidtracer6@gmail.com>
2023-07-08 22:35:53 -0400

    ./scr/HookApp.tsx: now the component FocusScreen is being rendered

--- src/HookApp.tsx
+++ src/HookApp.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 // import { CounterCustomHook } from './components/useState/counterCustomHook.tsx';
 // import { SimpleForm } from './components/useEffect/SimpleForm.tsx';
 // import { CustomHookSimpleForm } from './components/useEffect/CustomHookSimpleForm.tsx';
-import MultipleCustomHooksComponent from './components/MultipleCustomHooks/MultipleCustomHooks.tsx';
+//import MultipleCustomHooksComponent from './components/MultipleCustomHooks/MultipleCustomHooks.tsx';
+import FocusScreen from './components/useRef/FocusScreen.tsx';
 
 const HookApp = () => {
 
   return (
     <div>
-      <MultipleCustomHooksComponent />
+      <FocusScreen />
     </div>
   )
 }
