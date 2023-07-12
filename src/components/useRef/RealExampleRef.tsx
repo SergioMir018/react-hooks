824c1f1cadff0a048db04f2d4d50492c2016f89a
Sergio Mir <voidtracer6@gmail.com>
2023-07-12 10:01:57 -0400

    ./scr/components/useRef/RealExampleRef.tsx: MOD
    the component MultipleCustomHooks is now being rendered in this component

--- src/components/useRef/RealExampleRef.tsx
+++ src/components/useRef/RealExampleRef.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import MultipleCustomHooks from '../MultipleCustomHooks/MultipleCustomHooks'
 
 function RealExampleRef() {
   return (
-    <div className='text-white flex justify-center'>
+    <div className='text-white flex flex-col items-center'>
         <h1 className='text-5xl mt-4'>
             Ref Real Case 
         </h1>
+
+        <MultipleCustomHooks />
     </div>
   )
 }
